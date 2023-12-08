4f16202e13258fef900ad25ac73565326a87e6f7
Abhay Dutt <dutt.abhay@gmail.com>
2023-12-08 10:42:40 +0530

    setup singleProduct page

--- src/pages/SingleProduct.jsx
+++ src/pages/SingleProduct.jsx
@@ -10,6 +10,9 @@ const SingleProduct = () => {
   const [productColor, setProductColor] = useState(colors[0]);
   //state to handle the amount
   const [amount, setAmount] = useState();
+  const handleAmount = (event) => {
+    setAmount(parseInt(event.target.value));
+  };
   return (
     <section>
       <div className="text-md breadcrumbs">
@@ -21,53 +24,73 @@ const SingleProduct = () => {
             <Link to="/products">products</Link>
           </li>
         </ul>
-        {/* product */}
-        <div className="">
-          {/* image */}
-          <img src={image} alt={title} />
-          {/* product info */}
-          <div>
-            <h1>{title}</h1>
-            <h1>{company}</h1>
-            <p>{dollarAmount}</p>
-            <p>
-              Introducing the {title}, a beacon of medieval-inspired elegance.
-              Immerse yourself in the timeless allure of this enchanting piece,
-              featuring a wrought-iron base and intricate detailing. Emitting a
-              warm, flickering glow reminiscent of candlelight, the Regal Torch
-              adds a touch of old-world charm to any space. With its classic
-              design and sturdy craftsmanship, this lamp is not just a source of
-              light but a statement piece that transports you to a bygone era.
-              Illuminate your surroundings with the medieval mystique of the{" "}
-              {title}.
-            </p>
-            {/* colors */}
-            <div>
-              <h4>colors</h4>
-              <div>
-                {colors.map((color) => {
-                  return (
-                    <button
-                      key={color}
-                      type="button"
-                      style={{ backgroundColor: color }}
-                      onClick={() => setProductColor(color)}
-                    ></button>
-                  );
-                })}
-              </div>
+      </div>
+      {/* product */}
+      <div className="mt-6 grid gap-y-8 md:grid-cols-2  md:gap-x-16">
+        {/* image */}
+        <img
+          src={image}
+          alt={title}
+          className="w-96 h-96 object-cover rounded-lg lg:w-full"
+        />
+        {/* product info */}
+        <div>
+          <h1 className=" capitalize font-bold text-3xl">{title}</h1>
+          <h1 className=" mt-2 text-xl text-neutral-content font-bold">
+            {company}
+          </h1>
+          <p className=" mt-2 text-xl">{dollarAmount}</p>
+          <p className=" mt-6 leading-8">
+            Introducing the {title}, a beacon of medieval-inspired elegance.
+            Immerse yourself in the timeless allure of this enchanting piece,
+            featuring a wrought-iron base and intricate detailing. Emitting a
+            warm, flickering glow reminiscent of candlelight, the Regal Torch
+            adds a touch of old-world charm to any space. With its classic
+            design and sturdy craftsmanship, this lamp is not just a source of
+            light but a statement piece that transports you to a bygone era.
+            Illuminate your surroundings with the medieval mystique of the{" "}
+            {title}.
+          </p>
+          {/* colors */}
+          <div className=" mt-2">
+            <h4 className=" capitalize text-md tracking-wide font-medium">
+              colors
+            </h4>
+            <div className="">
+              {colors.map((color) => {
+                return (
+                  <button
+                    key={color}
+                    type="button"
+                    className={`badge w-6 h-6 mr-2 ${
+                      color == productColor && "border-2 border-secondary"
+                    }`}
+                    style={{ backgroundColor: color }}
+                    onClick={() => setProductColor(color)}
+                  ></button>
+                );
+              })}
             </div>
-            {/* Amount */}
-            <div>
-              <label> amount</label>
-              <select>
-                <option value={1}>1</option>
-                <option value={2}>2</option>
-                <option value={3}>3</option>
-              </select>
-            </div>
-            {/* cart button */}
-            <div onClick={() => console.log("added to bag")}>Add to bag</div>
+          </div>
+          {/* Amount */}
+          <div className=" form-control w-full max-w-xs">
+            <label className="label font-medium capitalize">amount</label>
+            <select
+              value={amount}
+              onChange={handleAmount}
+              className=" select select-secondary select-bordered select-md"
+            >
+              <option value={1}>1</option>
+              <option value={2}>2</option>
+              <option value={3}>3</option>
+            </select>
+          </div>
+          {/* cart button */}
+          <div
+            className=" mt-10 btn btn-secondary "
+            onClick={() => console.log("added to bag")}
+          >
+            ADD TO BAG
           </div>
         </div>
       </div>
